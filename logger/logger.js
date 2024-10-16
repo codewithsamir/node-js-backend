b2a8128720c0697686a037507450e9258e5c1342
samir rain <124909580+codewithsamir@users.noreply.github.com>
2024-10-16 19:53:13 +0545

    Update logger.js
    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -5,5 +5,30 @@ const EventEmitter = require('events')
 
 
 class Logger extends EventEmitter {
-    
+    log(message){
+        this.emit('message',{message});
+    }
 }
+
+
+const logger = new Logger()
+
+const logfile = './logger/eventlog.txt';
+
+const logTofile = (event)=>{
+    const logmessage = `${new Date().toISOString()} - ${event.message} \n`
+    fs.appendFileSync(logfile, logmessage)
+}
+
+
+ logger.on('message',logTofile)
+setInterval(
+    ()=>{
+        const memoryUsage = os.freemem() / os.totalmem() * 100
+        logger.log(`current memory usage: ${memoryUsage.toFixed(2)}`)  
+    }
+    ,3000)
+
+    logger.log('Application Started');
+    logger.log('Application occured');
+    
