0a9a4afabe7406f14c456004a6814e5f35832ce6
samir rain <124909580+codewithsamir@users.noreply.github.com>
2024-12-20 11:23:22 +0000

    again update

--- todo/src/controllers/user.controllers.js
+++ todo/src/controllers/user.controllers.js
@@ -174,7 +174,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: { refreshToken: undefined },
+      $unset: { refreshToken: 1 } // this removes the field from document
     },
     {
       new: true,
